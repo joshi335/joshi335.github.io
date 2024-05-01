ebf6f4334ad874fca7aa4195401b8ad6e7484f9a
joshi335 <157381523+joshi335@users.noreply.github.com>
2024-05-01 12:47:41 +0000

    Finished Todos

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -49,25 +49,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            physikz.updatePosition(circles[0]); 
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
+            physikz.updatePosition(circle)
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
             
-            game.checkCirclePosition(circles[0]); 
-            game.checkCirclePosition(circles[1]); 
-            game.checkCirclePosition(circles[2]); 
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);  
+        
 
 
             // TODO 9 : Iterate over the array
            
-            
+                for (var i = 0; i < circles.length; i++) {
+                    physikz.updatePosition(circles[i])
+                    game.checkCirclePosition(circles[i])
+                }
         }
     
         /* 
@@ -96,7 +90,7 @@ var init = function (window) {
                 circle.y = canvas.height; 
             }
 
-            var rightEdge = circle.x + circle.radius; 
+            
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
